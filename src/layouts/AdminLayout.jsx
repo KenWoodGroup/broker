5a83350c40946645560642f084b53c8c30c6a699
jurayevd3v <jurayevd3v@gmail.com>
2026-04-08 14:29:20 +0500

    dev lead factory

--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -5,15 +5,17 @@ import { Building2, Factory, Home, Settings2, Users2 } from "lucide-react";
 import { Box } from "@chakra-ui/react";
 
 const links = [
-    { label: "Home", to: "/", icon: Home },
+    { label: "Bosh sahifa", to: "/", icon: Home },
     // { label: "Operatorlar", to: "/operators", icon: Headset },
     // { label: "Brokerlar", to: "/brokers", icon: User, end: true },
-    { label: "Hodimlar", to: "/roles", icon: Users2, end: false },
-    { label: "Factory", to: "/factories", icon: Factory },
-    { label: "Companies", to: "/companies", icon: Building2 },
+    { label: "Xodimlar", to: "/roles", icon: Users2, end: false },
+    { label: "T-zavodlar", to: "/lead-factory", icon: Factory },
+    { label: "Zavodlar", to: "/factories", icon: Factory },
+    { label: "Qurilish kompaniyalar", to: "/companies", icon: Building2 },
     { label: "Opsiyalar", to: "/options", icon: Settings2 },
-    // { label: "Products", to: "/products", icon: Boxes },
+    // { label: "Mahsulotlar", to: "/products", icon: Boxes },
 ];
+
 export default function AdminLayout() {
     const { collapsed } = useUIStore();
     return (
--- src/utils/routes/adminRoutes.jsx
+++ src/utils/routes/adminRoutes.jsx
@@ -12,6 +12,7 @@ import SPoperators from "../../pages/SPoperators/SPoperators";
 import ADhome from "../../pages/ADhome/ADhome";
 import RolesPage from "../../pages/ADworkers/ADworkers";
 import ADsuppliers from "../../pages/ADsuplliers/ADsuppliers";
+import LeadFactory from "../../pages/LeadFactory";
 
 const adminRoutes = [
     {
@@ -73,7 +74,13 @@ const adminRoutes = [
         name:'roles',
         path:'roles',
         element:<RolesPage/>
+    },
+        {
+        name:'lead-factory',
+        path:'lead-factory',
+        element:<LeadFactory/>
     }
+
 
 ];
 
