6a923239c075478b8640b4a66305dac16a48d3f1
farxod-0017 <farxodegamnazarov2@gamil.com>
2026-04-23 10:00:38 +0500

    ok

--- src/utils/Controllers/OfferItems.js
+++ src/utils/Controllers/OfferItems.js
@@ -5,8 +5,16 @@ class apiOfferItems {
         const response = await $api.get(`${BASE_URL}/api/offer-items`)
         return response;
     }
+    static PostVariants = async (data) => {
+        const response = await $api.post(`${BASE_URL}/api/offer-items/variants`, data)
+        return response;
+    }
     static getByUser = async (status, page, limit) => {
         const response = await $api.get(`${BASE_URL}/api/offer-items/supplier?status=${status}&page=${page}&limit=${limit}`)
+        return response;
+    }
+    static getById = async (id) => {
+        const response = await $api.get(`${BASE_URL}/api/offer-items/bu-id/${id}`)
         return response;
     }
     static updateStatus = async (id, data) => {
